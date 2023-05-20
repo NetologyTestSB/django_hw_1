656f23d308423efc0fe486d6591abfeb0892eecb
sb56@volny.cz <NetologyTestSB>
2023-05-20 18:10:34 +0200

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,41 @@
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from  datetime import datetime
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': '',
+        'Показать содержимое рабочей директории': ''
+    }
+    
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    # обратите внимание – здесь HTML шаблона нет, 
+    # возвращается просто текст
+    current_time = datetime.today().strftime('%d.%m.%Y %H:%M:%S')
+    #current_time = None
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    # по аналогии с `time_view`, напишите код,
+    # который возвращает список файлов в рабочей 
+    # директории
+    dirlist = os.listdir()
+    return HttpResponse(dirlist)
+    #raise NotImplemented
